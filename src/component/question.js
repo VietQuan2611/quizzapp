09ed96c28d4e2bd1b7253b6967d9e24daf413fc2
Luong Mau viet Quan <vietquan2611@gmail.com>
2023-06-08 09:02:32 +0700

    quizzapp

---
+++ src/component/question.js
@@ -0,0 +1,98 @@
+const questions = [
+    {
+        question: 'Ban nhạc ABBA nổi tiếng với bài hát Happy New Year là ban nhạc của nước nào?',
+        answers: ['Mỹ', 'Anh', 'Thuỵ Điển', 'Scotland'],
+        correctAnswer: 2,
+    },
+    {
+        question: 'Hệ số thập phân là phát minh của người nước nào? ',
+        answers: ['Ấn Độ', 'Ai Cập', 'Áo', 'Pháp'],
+        correctAnswer: 0,
+    },
+    {
+        question: 'Thành phần hoá học chủ yếu của tro bếp là nguyên tố nào? ',
+        answers: ['Cacbon', 'Kali', 'Nito', 'Magie'],
+        correctAnswer: 1,
+    },
+    {
+        question: 'Hành tinh nào trong hệ mặt trời có cấu tạo giống trái đất nhất? ',
+        answers: ['Sao Kim', 'Sao Hoả', 'Mặt Trăng', 'Sao Thổ'],
+        correctAnswer: 1,
+    },
+    {
+        question: 'Có bao nhiêu số chẵn có hai chữ số khác nhau? ',
+        answers: ['45', '40', '43', '41'],
+        correctAnswer: 3,
+    },
+    {
+        question: 'Quốc gia nào có diện tích lớn thứ hai thế giới? ',
+        answers: ['Nga', 'Trung Quốc', 'Canada', 'Senegal'],
+        correctAnswer: 2,
+    },
+    {
+        question: 'Tổng thống Ukraine Volodymyr Zelensky làm nghề gì trước khi nhậm chức? ',
+        answers: ['Doanh Nhân', 'Bác Sĩ', 'Kỹ Sư Điện', 'Diễn Viên Hài'],
+        correctAnswer: 3,
+    },
+    {
+        question: 'Chiến trường Đắk Tô - Tân Cảnh, nơi diễn ra chiến thắng vang đội năm 1972, nay thuộc địa bàn tỉnh nào ở Tây Nguyên? ',
+        answers: ['Kon Tum', 'Đăk Lăk', 'Đăk Nông', 'Gia Lai'],
+        correctAnswer: 0,
+    },
+    {
+        question: 'Nhạc sĩ nào là người phổ nhạc ca khúc "Cây đàn sinh viên"? ',
+        answers: ['Quốc An', 'Bảo Chấn', 'Trịnh Công Sơn', 'Trần Tiến'],
+        correctAnswer: 0,
+    },
+    {
+        question: 'Hoa hậu Hòa bình Quốc tế 2017 đã được tổ chức tại quốc gia nào? ',
+        answers: ['Lào', 'Thái Lan', 'Campuchia', 'Việt Nam'],
+        correctAnswer: 3,
+    },
+    {
+        question: 'Đền Bát Đế thờ 8 vị vua nhà Lý thuộc tỉnh nào? ',
+        answers: ['Thái Nguyên', 'Hà Nội', 'Ninh Bình', 'Bắc Ninh'],
+        correctAnswer: 3,
+    },
+    {
+        question: 'Việt Nam là thành viên thứ bao nhiêu của Liên hợp quốc? ',
+        answers: ['135', '149', '127', '141'],
+        correctAnswer: 1,
+    },
+    {
+        question: 'Đặng Thái Sơn được giải nhất piano quốc tế Chopin vào năm nào? ',
+        answers: ['1983', '1981', '1980', '1982'],
+        correctAnswer: 2,
+    },
+    {
+        question: 'Chuồn chuồn có bao nhiêu răng? ',
+        answers: ['Không có răng', '16', '32', '12'],
+        correctAnswer: 1,
+    },
+    {
+        question: 'Loại cỏ nào đã cứu sống Dế Mèn và Dế Trũi khỏi chết đói sau những ngày lênh đênh trên sông nước? ',
+        answers: ['Cỏ Chỉ', 'Cỏ Mây', 'Cỏ Nước', 'Cỏ Lác'],
+        correctAnswer: 2,
+    },
+    {
+        question: 'Cấu tạo chủ yếu của sao chổi là gì? ',
+        answers: ['Thiên Thạch', 'Cacbon', 'Băng', 'Đá Vôi'],
+        correctAnswer: 2,
+    },
+    {
+        question: 'Trong những người sau đây, người nào không phải là 1 trong tứ đại mỹ nhân của Trung QUốc? ',
+        answers: ['Triệu Cơ', 'Tây Thi', 'Điêu Thuyền', 'Vương Chiêu Quân'],
+        correctAnswer: 0,
+    },
+    {
+        question: 'Đại dương nào nhỏ nhất thế giới? ',
+        answers: ['Thái Bình Dương', 'Đại Tây Dương', 'Ấn Độ Dương', 'Bắc Băng Dương'],
+        correctAnswer: 0,
+    },
+    {
+        question: 'Ánh sáng từ Mặt trời tới trái đất mất khoảng bao nhiêu phút? ',
+        answers: ['6 phút', '8 phút', '10 phút', '4 phút'],
+        correctAnswer: 1,
+    },
+];
+ export default questions;
